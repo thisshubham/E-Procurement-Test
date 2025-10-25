e4a0458653f92cfcfcf34b28b688213d6cbbf01a
Shubh <shubhaampatel2609@gmail.com>
2025-10-25 23:37:54 +0530

    Question controller

---
+++ E-Procurement-Services/src/main/java/com/eProcurement/dto/StudentDto.java
@@ -0,0 +1,27 @@
+package com.eProcurement.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDto {
+    private Long id;
+
+    private String name;
+    private String email;
+    private String role;
+    private String password;
+    private String userName;
+
+    private String studentId;
+    private String enrollmentYear;
+
+    private Long departmentId;
+    private String departmentName;
+
+    private Long teacherId;
+    private String teacherName;
+    private String userRole;
+}
